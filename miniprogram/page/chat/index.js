4f1897e47c5ea014a857d1ab783fb82c55015664
lin-dongyizhi7 <2985956026@qq.com>
2024-09-29 20:33:23 +0800

    Initial Commit

---
+++ miniprogram/page/chat/index.js
@@ -0,0 +1,53 @@
+Page({
+  onShow() {
+    wx.reportAnalytics('enter_home_programmatically', {})
+
+    // http://tapd.oa.com/miniprogram_experiment/prong/stories/view/1020425689866413543
+    if (wx.canIUse('getExptInfoSync')) {
+      console.log('getExptInfoSync expt_args_1', wx.getExptInfoSync(['expt_args_1']))
+      console.log('getExptInfoSync expt_args_2', wx.getExptInfoSync(['expt_args_2']))
+      console.log('getExptInfoSync expt_args_3', wx.getExptInfoSync(['expt_args_3']))
+    }
+    if (wx.canIUse('reportEvent')) {
+      wx.reportEvent('expt_event_1', {expt_data: 1})
+      wx.reportEvent('expt_event_2', {expt_data: 5})
+      wx.reportEvent('expt_event_3', {expt_data: 9})
+      wx.reportEvent('expt_event_4', {expt_data: 200})
+
+      wx.reportEvent('weexpt_event_key_1', {option_1: 1, option_2: 10, option_str_1: 'abc'})
+      wx.reportEvent('weexpt_event_key_1', {option_1: 'abc', option_2: '1000', option_str_1: '1'})
+    }
+  },
+  onShareAppMessage() {
+    return {
+      title: '聊天',
+      path: 'page/chat/index'
+    }
+  },
+  onShareTimeline() {
+    '聊天'
+  },
+
+  data: {
+    list: [
+      '你好'
+    ],
+    theme: 'light'
+  },
+
+  onLoad() {
+    this.setData({
+      theme: wx.getSystemInfoSync().theme || 'light'
+    })
+
+    if (wx.onThemeChange) {
+      wx.onThemeChange(({theme}) => {
+        this.setData({theme})
+      })
+    }
+  },
+
+  kindToggle(e) {
+    const id = e.currentTarget.id
+  }
+})
